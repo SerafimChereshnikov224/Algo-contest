65d235e2505ea62b852fb9033d0fac5685ed7bf7
Daniil Kornishev <doddadid@gmail.com>
2025-03-21 17:53:14 +0300

    2

---
+++ S2.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class S2 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int solve(int num) {
+        // 1. число в двоичную систему .
+        // 2. если 3 единицы возвращаем сумму степеней 2 в позициях первых трех единиц
+        // 3. если меньше 3 , то вычетаем 1 и чекаем пункт 2)
+        // если число стало меньше 7 то нафиг
+        return -1;
+    }
+}
