c6f50925bcf560007ffa990c09a20a645bca5fd0
Daniil Kornishev <doddadid@gmail.com>
2025-03-23 13:56:48 +0300

    solved 2

--- S2.java
+++ S2.java
@@ -1,9 +1,14 @@
 package org.example;
 
+import java.util.Scanner;
+import java.util.stream.Stream;
+
 public class S2 {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println(solve(Integer.parseInt(str)));
     }
 
     public static int solve(int num) {
@@ -11,6 +16,23 @@ public class S2 {
         // 2. если 3 единицы возвращаем сумму степеней 2 в позициях первых трех единиц
         // 3. если меньше 3 , то вычетаем 1 и чекаем пункт 2)
         // если число стало меньше 7 то нафиг
-        return -1;
+
+        if(num < 7) return -1;
+
+        int res = 0;
+
+        var bNum = Integer.toBinaryString(num);
+        int oneCount = 0;
+        int position = bNum.length() - 1;
+        for(char ch : bNum.toCharArray()) {
+            if(ch == '1') {
+                oneCount++;
+                res += (int) Math.pow(2,position);
+            }
+            position--;
+            if(oneCount == 3) return res;
+        }
+
+        return solve(num - 1);
     }
 }
